498ca2812cb6d4745547ca76eefa02937b5dab0d
Terex12 <sz.yufanxu@gmail.com>
2021-07-04 12:44:46 -0600

    mem profile for default checkpointing

---
+++ uu/test/checkpoint_reference.py
@@ -0,0 +1,114 @@
+import torch
+from uu.utils import memory 
+from torch.utils.checkpoint import checkpoint
+from torch.nn.parameter import Parameter
+
+class Net(torch.nn.Module):
+    def __init__(self, m, n, k, l) :
+        super(Net, self).__init__()
+        self.linear_1 = torch.nn.Linear(k, n, bias=False) # weight: 240KB  200 300
+        self.linear_2 = torch.nn.Linear(n, l, bias=False) # weight: 840KB  300 700
+        self.linear_3 = torch.nn.Linear(l, l, bias=False) # weight:  ~1.9 MB 700 700
+        self.linear_4 = torch.nn.Linear(l, 1000, bias=False) # weight:  2.8MB 700 1000
+        self.linear_5 = torch.nn.Linear(1000, 2000, bias=False) # weight:  8MB 1000 2000
+
+        self.li3 = torch.nn.Sequential(*[self.linear_2, self.linear_3, self.linear_4])
+
+        self.dummy_param = torch.nn.Parameter(torch.empty(0))
+        
+
+    
+
+    def forward(self, x, checkpointing=True):    # [400, 200] 320K
+        device = self.dummy_param.device
+        print("device ", device)
+        memUsage = memory.MeasureMemory(device)
+        if checkpointing:
+            print("==== init ...")
+            print(memUsage.snapshot())
+            print(memUsage.currentValue())      #init now should be around 6MB+8
+            print(memUsage.current())
+            x = self.linear_1(x)                #480K [400, 300] 
+            print("==== 1st lin ...")
+            print(memUsage.snapshot())          #+480K = 6.5M+8
+            print(memUsage.currentValue())      
+            print(memUsage.current())
+
+
+            x = checkpoint(self.li3, x)         # should not save out2, out3 (1.12 x 2)
+                                                #out 1.6M [400, 1000]
+            print("==== checkp 3 lin ...")
+            print(memUsage.snapshot())          
+            print(memUsage.currentValue())      #+1.6M = 8.1M+8
+            print(memUsage.current())
+
+            x = self.linear_5(x)                #3.2M [400, 2000]
+            print("==== last lin ...")
+            print(memUsage.snapshot())
+            print(memUsage.currentValue())      #+3.2M = 11.3+8
+            print(memUsage.current())
+        else:
+            print("==== init ...")
+            print(memUsage.snapshot())
+            print(memUsage.currentValue())      #init now should be around 6MB+8
+            print(memUsage.current())
+            
+            x = self.linear_1(x)
+            print("==== 1st lin ...")           #480K [400, 300]
+            print(memUsage.snapshot())
+            print(memUsage.currentValue())      #+480K = 6.5M+8
+            print(memUsage.current())
+            
+            x = self.linear_2(x)                #1.12M [400, 700]
+            print("==== 2nd lin ...")
+            print(memUsage.snapshot())
+            print(memUsage.currentValue())      #+1.12M = 7.6M+8
+            print(memUsage.current())
+            
+            x = self.linear_3(x)                #1.12M [400, 700]
+            print("==== 3rd lin ...")
+            print(memUsage.snapshot())
+            print(memUsage.currentValue())      #+1.12M = 8.7M+8
+            print(memUsage.current())
+            
+            x = self.linear_4(x)                #1.6M [400, 1000]
+            print("==== 4th lin ...")
+            print(memUsage.snapshot())
+            print(memUsage.currentValue())      #+1.6M = 10.3 M+8
+            print(memUsage.current())
+
+            x = self.linear_5(x)                #3.2M [400, 2000]
+            print("==== 5th lin ...")
+            print(memUsage.snapshot())
+            print(memUsage.currentValue())      #+3.2M = 13.5 M+8
+            print(memUsage.current())
+
+        return x
+
+def main():
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    memUsage = memory.MeasureMemory(device)
+
+
+
+    m = 400
+    n = 300
+    k = 200
+    l = 700
+
+    model = Net(m, n, k, l).to(device)
+    input = torch.rand(m, k, requires_grad = True).cuda() # [400, 200] 320K
+    out = model(input, False)
+
+
+
+    print("print(out)", out)
+
+
+
+
+if __name__=="__main__":
+    main()
+
+
--- uu/test/prototype_load_GPU.py
+++ uu/test/prototype_load_GPU.py
@@ -11,117 +11,6 @@ def printsize(self, grad_input, grad_output):
     print('grad_output size : ', grad_output[0].size())
 
 
-class Net(torch.nn.Module):
-    def __init__(self, m, n, k, l) :
-
-        #[m, k] * [k, n] --> [m, n]*[n, l] --> [m, l]
-        super(Net, self).__init__()
-        self.linear_1 = torch.nn.Linear(k, n) # weight: 240KB  200 300
-        self.linear_2 = torch.nn.Linear(n, l) # weight: 840KB  300 700
-
-        # self.linear_1.weight = Parameter(torch.reshape(torch.arange(0, k*n, step=1.0, dtype=torch.float), (n, k)))
-        # self.linear_2.weight = Parameter(torch.reshape(torch.arange(0, -l*n, step=-1.0, dtype=torch.float), (l, n)))  
-        # self.linear_1.bias = Parameter(torch.arange(0, n, step=1.0, dtype=torch.float))
-        # self.linear_2.bias = Parameter(torch.arange(0, -l, step=-1.0, dtype=torch.float))  
-
-
-        self.linear_1.register_full_backward_hook(printsize)
-        self.linear_2.register_full_backward_hook(printsize)
-        
-        print("==== linear 1 weight shape", self.linear_1.weight.size())
-        print("++++ linear 1 weight ", self.linear_1.weight)
-        print("==== linear 2 weight shape", self.linear_2.weight.size())
-        print("++++ linear 2 weight ", self.linear_2.weight)
-        self.dummy_param = torch.nn.Parameter(torch.empty(0))
-       
-
-    def forward(self, x):
-        device = self.dummy_param.device
-        print("device ", device)
-        memUsage = memory.MeasureMemory(device)
-
-
-        #temp = torch.split(x, 100, dim=0) # 320K
-
-        temp0 = torch.narrow(x, dim=0, start=0, length=100)
-        temp1 = torch.narrow(x, 0, 100, 300)
-        # temp0 = torch.narrow(x, dim=0, start=0, length=1)
-        # temp1 = torch.narrow(x, 0, 1, 3)
-
-        # print("==== after split input, will double space?? ...") # looks not
-        # print(memUsage.currentValue())
-        # print(memUsage.availableValue())
-        # print(memUsage.current())
-        # print(memUsage.available())
-        # print(memUsage.maxx())
-        # print(memUsage.currentCached())
-        
-        result_1 = self.linear_1(temp0)
-        # print("==== result_1 shape", result_1.size())
-        # print("==== after first Linear ...") # res1 [100*300]
-        # print(memUsage.currentValue())
-        # print(memUsage.availableValue())
-        # print(memUsage.current())
-        # print(memUsage.available())
-        # print(memUsage.maxx())
-        # print(memUsage.currentCached())
-
-        
-        result_2 = self.linear_1(temp1)
-        # print("==== result_2 shape", result_2.size())
-        # print("==== after first Linear ...") # res2 [300*300]        
-        # print(memUsage.currentValue())
-        # print(memUsage.availableValue())
-        # print(memUsage.current())
-        # print(memUsage.available())
-        # print(memUsage.maxx())
-        # print(memUsage.currentCached())
-
-        f_res1 = self.linear_2(result_1) #[100*700]
-        f_res2 = self.linear_2(result_2) #[300*700]
-        out_list = [f_res1, f_res2]
-        out = torch.cat(out_list, dim=0)
-        return out
-
-
-
-class Net_ref(torch.nn.Module):
-    def __init__(self, m, n, k, l, l1_w, l2_w, l1_b, l2_b) :
-
-        #[m, k] * [k, n] --> [m, n]*[n, l] --> [m, l]
-        super(Net_ref, self).__init__()
-        self.linear_1 = torch.nn.Linear(k, n) # weight: 240KB  200 300
-        self.linear_2 = torch.nn.Linear(n, l) # weight: 840KB  300 700
-
-        # self.linear_1.weight = Parameter(torch.reshape(torch.arange(0, k*n, step=1.0, dtype=torch.float), (n, k)))
-        # self.linear_2.weight = Parameter(torch.reshape(torch.arange(0, -l*n, step=-1.0, dtype=torch.float), (l, n)))  
-        # self.linear_1.bias = Parameter(torch.arange(0, n, step=1.0, dtype=torch.float))
-        # self.linear_2.bias = Parameter(torch.arange(0, -l, step=-1.0, dtype=torch.float))  
-
-        self.linear_1.weight = l1_w
-        self.linear_2.weight = l2_w
-        self.linear_1.bias = l1_b
-        self.linear_2.bias = l2_b
-
-        self.linear_1.register_full_backward_hook(printsize)
-        self.linear_2.register_full_backward_hook(printsize)
-        
-        print("==== ref linear 1 weight shape", self.linear_1.weight.size())
-        print("++++ ref linear 1 weight ", self.linear_1.weight)
-        print("==== ref linear 2 weight shape", self.linear_2.weight.size())
-        print("++++ ref linear 2 weight ", self.linear_2.weight)
-        self.dummy_param = torch.nn.Parameter(torch.empty(0))
-       
-
-    def forward(self, x):
-        device = self.dummy_param.device
-        print("device ", device)
-        memUsage = memory.MeasureMemory(device)
-
-        out = self.linear_2(self.linear_1(x))
-
-        return out
-
 class test_Net_ref(torch.nn.Module):
     def __init__(self) :
         super(test_Net_ref, self).__init__()
@@ -129,83 +18,83 @@ class test_Net_ref(torch.nn.Module):
         
     
     def forward(self, x):
-        with torch.no_grad():
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            memUsage = memory.MeasureMemory(device)
-            print("==== init ...")
-            print(memUsage.snapshot())
-            print(memUsage.currentValue())      #now should be around 2MB
-            print(memUsage.availableValue())
-            # print(memUsage.current())
-            # print(memUsage.available())
+        #with torch.no_grad():
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        memUsage = memory.MeasureMemory(device)
+        print("==== init ...")
+        print(memUsage.snapshot())
+        print(memUsage.currentValue())      #now should be around 2MB
+        print(memUsage.availableValue())
+        # print(memUsage.current())
+        # print(memUsage.available())
 
-            temp = x[0:19, :]        
-            #print("temp", temp)
-            
-            print("==== ref slicing ...")
-            print(memUsage.snapshot())
-            print(memUsage.currentValue())       #shoud no change
-            print(memUsage.availableValue())
-            # print(memUsage.current())
-            # print(memUsage.available())
+        temp = x[0:19, :]        
+        #print("temp", temp)
+        
+        print("==== ref slicing ...")
+        print(memUsage.snapshot())
+        print(memUsage.currentValue())       #shoud no change
+        print(memUsage.availableValue())
+        # print(memUsage.current())
+        # print(memUsage.available())
 
-            tt = temp.to(device)  # 20x1000 80KB
-            
+        tt = temp.to(device)  # 20x1000 80KB
+        
 
-            print("==== send slice ...")
-            print(memUsage.snapshot())
-            print(memUsage.currentValue())      #shoud +80K
-            print(memUsage.availableValue())
-            # print(memUsage.current())
-            # print(memUsage.available())
+        print("==== send slice ...")
+        print(memUsage.snapshot())
+        print(memUsage.currentValue())      #shoud +80K
+        print(memUsage.availableValue())
+        # print(memUsage.current())
+        # print(memUsage.available())
 
-            #print("send temp to cuda", temp)
-            out1 = self.linear(tt)    # 20 x 500 40KB
-            #print("out", out1)
-            print("==== output slice ...")
-            print(memUsage.snapshot())
-            print(memUsage.currentValue())      #should +40KB
-            print(memUsage.availableValue())
-            # print(memUsage.current())
-            # print(memUsage.available())
-
-
-            
-            del tt    
-            #del out1
-            print("==== after del temp buffer ...")
-            print(memUsage.snapshot())
-            print(memUsage.currentValue())      #should -80KB
-            print(memUsage.availableValue())
-            # print(memUsage.current())
-            # print(memUsage.available())
+        #print("send temp to cuda", temp)
+        out1 = self.linear(tt)    # 20 x 500 40KB
+        #print("out", out1)
+        print("==== output slice ...")
+        print(memUsage.snapshot())
+        print(memUsage.currentValue())      #should +40KB
+        print(memUsage.availableValue())
+        # print(memUsage.current())
+        # print(memUsage.available())
 
 
-            temp = x[20:39, :]
-            print("==== 2nd slicing ...")
-            print(memUsage.currentValue())       #shoud no change
-            print(memUsage.availableValue())
-            # print(memUsage.current())
-            # print(memUsage.available())
-
-            tt = temp.to(device='cuda')  # # 20x1000 80KB
-            print("==== send 2nd slice ...")
-            print(memUsage.snapshot())
-            print(memUsage.currentValue())      #shoud +80K
-            print(memUsage.availableValue())
-            # print(memUsage.current())
-            # print(memUsage.available())
-
-            out2 = self.linear(tt)
-            print("==== output slice ...")
-            print(memUsage.snapshot())
-            del tt
+        
+        del tt    
+        #del out1
+        print("==== after del temp buffer ...")
+        print(memUsage.snapshot())
+        print(memUsage.currentValue())      #should -80KB
+        print(memUsage.availableValue())
+        # print(memUsage.current())
+        # print(memUsage.available())
 
 
-            print("==== final ...")
-            del out1
-            del out2
-            print(memUsage.snapshot())
+        temp = x[20:39, :]
+        print("==== 2nd slicing ...")
+        print(memUsage.currentValue())       #shoud no change
+        print(memUsage.availableValue())
+        # print(memUsage.current())
+        # print(memUsage.available())
+
+        tt = temp.to(device='cuda')  # # 20x1000 80KB
+        print("==== send 2nd slice ...")
+        print(memUsage.snapshot())
+        print(memUsage.currentValue())      #shoud +80K
+        print(memUsage.availableValue())
+        # print(memUsage.current())
+        # print(memUsage.available())
+
+        out2 = self.linear(tt)
+        print("==== output slice ...")
+        print(memUsage.snapshot())
+        del tt
+
+
+        print("==== final ...")
+        del out1
+        del out2
+        print(memUsage.snapshot())
 
 
         
@@ -216,86 +105,6 @@ def main():
     model = test_Net_ref().cuda()
 
     model(input)
-
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # memUsage = memory.MeasureMemory(device)
-    # # print("==== init ...")
-    # # print(memUsage.currentValue())
-    # # print(memUsage.availableValue())
-    # # print(memUsage.current())
-    # # print(memUsage.available())
-    # # print(memUsage.maxx())
-    # # print(memUsage.currentCached())
-
-    # m = 400
-    # n = 300
-    # k = 200
-    # l = 700
-
-    # # m = 4
-    # # n = 3
-    # # k = 2
-    # # l = 7
-    # model = Net(m, n, k, l).to(device)
-    # #model_ref = Net_ref(m, n, k, l).to(device)
-
-    # l1_w = model.linear_1.weight
-    # l2_w = model.linear_2.weight
-    # l1_b = model.linear_1.bias
-    # l2_b = model.linear_2.bias
-    # # use same random as Net()
-    # model_ref = Net_ref(m, n, k, l, l1_w, l2_w, l1_b, l2_b).to(device)
-
-
-
-    # # print("==== after define network (weight size) ...")
-    # # print(memUsage.currentValue())
-    # # print(memUsage.availableValue())
-    # # print(memUsage.current())
-    # # print(memUsage.available())
-    # # print(memUsage.maxx())
-    # # print(memUsage.currentCached())
-
-
-
-    # input = torch.rand(m, k, requires_grad = True).cuda() # 320K
-    # # print("==== after allocate input ...")
-    # # print(memUsage.currentValue())
-    # # print(memUsage.availableValue())
-    # # print(memUsage.current())
-    # # print(memUsage.available())
-    # # print(memUsage.maxx())
-    # # print(memUsage.currentCached())
-
-
-    # out = model(input)
-    # out_ref = model(input)
-    # print("out shape", out.size())
-    # print("out_ref shape", out_ref.size())
-    # print("check forward correctness")
-    # assert(torch.all(torch.eq(out, out_ref)))
-
-    # out.sum().backward()
-    # out_ref.sum().backward()
-    
-    # out_w_list = []
-    # i = 0
-    # for name, param in model.named_parameters():
-    #     print("i", i, name, param.size())
-    #     i += 1
-    #     out_w_list.append(param)
-
-    # out_ref_w_list = []
-    # i = 0
-    # for name, param in model_ref.named_parameters():
-    #     print("i", i, name, param.size())
-    #     i += 1
-    #     out_ref_w_list.append(param)
-
-
-    # for ww in range(0, i):
-    #     print("out_w {} \n out_ref_w {}".format(out_w_list[ww], out_ref_w_list[ww]))
-    #     assert(torch.all(torch.eq(out_w_list[ww], out_ref_w_list[ww])))
 
 if __name__=="__main__":
     main()
